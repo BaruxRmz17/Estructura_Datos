901030f327b53bb8d57c348238e1448a6103101f
Baruc Ramirez <barux111706@gmail.com>
2026-02-11 14:03:29 -0600

    feat: Implement stack-based palindrome checker and decimal-to-binary conversion, and remove redundant empty check from `Pila.push`.

--- Listas/Listas_Clase_2.py
+++ Listas/Listas_Clase_2.py
@@ -26,8 +26,7 @@ class Pila:
         self.items = []
 
     def push(self , value):
-        if self.is_empy():
-            return None
+
         self.items.append(value)
         return self.items
     
@@ -59,3 +58,79 @@ print(pila.peek())
 print(pila.pop())
 print(pila.size())
 print(pila.is_empy())
+
+
+print("===================")
+print("Ejercicios 1")
+print("===================")
+
+class stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, value):
+        self.items.append(value)
+        return self.items
+
+    def size(self):
+        return len(self.items)
+    
+    def pop(self):
+        return self.items.pop()
+    
+def es_Palo(palabra ):
+        p = stack()
+        for letra in palabra:
+            p.push(letra)
+            
+        invertida = ""
+        print(p.size())
+        
+        while p.size() > 0:
+            invertida += p.pop()
+            
+        print(invertida)
+        return palabra == invertida
+
+
+        
+    
+palo = "ana"
+print(es_Palo(palo))
+
+
+
+print("===================")
+print("Ejercicios 2")
+print("===================")
+
+
+
+class NumBin:
+    def __init__(self):
+        self.items = []
+
+    def push(self, value):
+        self.items.append(value)
+        return self.items
+
+    def size(self):
+        return len(self.items)
+    
+    def pop(self):
+        return self.items.pop()
+    
+def binario(num):
+        p = stack()
+
+        while num > 0:
+            residuo = num % 2
+            num = num // 2
+            p.push(residuo)
+
+        binario = ""
+        while p.size() > 0:
+            binario += str(p.pop())
+        return binario
+
+print(binario(10))
